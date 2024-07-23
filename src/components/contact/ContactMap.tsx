4fb1863f1897fd388d8229be1a6cff2678c0a506
Suvom2024 <akash.barka@gmail.com>
2024-07-23 11:38:44 +0530

    Initial commit on new main branch

--- src/components/contact/ContactMap.tsx
+++ src/components/contact/ContactMap.tsx
@@ -3,7 +3,7 @@ const ContactMap = () => {
    return (
       <div className="contact-g-map">
          <iframe
-            src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d29208.601361499546!2d90.3598076!3d23.7803374!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sbd!4v1589109092857!5m2!1sen!2sbd"></iframe>
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d224345.83993901616!2d77.06889748825568!3d28.527280342326376!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390ce375636fdb1d%3A0x3e5c7f7b9b3151d7!2sDelhi%2C%20India!5e0!3m2!1sen!2sus!4v1626108609264!5m2!1sen!2sus"></iframe>
       </div>
    )
 }
