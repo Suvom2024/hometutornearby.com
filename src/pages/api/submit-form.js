b6bbe791928348fe9d6db6a7c75b1ebe51478cb3
Suvom2024 <akash.barka@gmail.com>
2024-09-16 11:13:51 +0530

    Slight Changes

--- src/pages/api/submit-form.js
+++ src/pages/api/submit-form.js
@@ -1,8 +1,11 @@
+// pages/api/submit-form.js
+
 import { google } from 'googleapis';
 
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
-    return res.status(405).json({ message: 'Method Not Allowed' });
+    res.status(405).json({ message: 'Method Not Allowed' });
+    return;
   }
 
   const { userType, name, mobileNumber, email, course, subject } = req.body;
@@ -48,7 +51,7 @@ export default async function handler(req, res) {
 
     res.status(200).json({ message: 'Success' });
   } catch (error) {
-    console.error('Error writing to Google Sheet:', error); // Add detailed error logging
-    res.status(500).json({ message: 'Internal Server Error', error: error.message });
+    console.error('Error writing to Google Sheet:', error);
+    res.status(500).json({ message: 'Internal Server Error' });
   }
 }
